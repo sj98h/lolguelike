641bb7a2dd872f4fe243413c99059c57d0e4adcf
sj98h <sj98h@naver.com>
2024-08-22 15:58:54 +0900

    플레이어 스킬 완성

--- game.js
+++ game.js
@@ -1,25 +1,27 @@
-/*
-toDo:
-1. 턴 수에 따라 지속되는 효과를 처리할 로직이 필요하다
-  eg. Q 스킬 이속 증가 효과가 2턴 뒤에 사라져야함 / n 턴 동안 슬로우가 묻는 스킬...
-2. 방어력, 이동 속도는 변동 후 원복되어야 한다. 따라서 같은 값을 참조하는 변수를 추가해야 한다.
-3. 소리가 나오면 좋겠다.
-4. 메시지 간 딜레이를 넣으면 좋겠다
-5. 점멸 (도망) 기능. 추가 기획 필요 체력이 50% 이하일 때만 사용 가능하게, 성공확률은 이속 공식을 참조하여 회피 확률과 동일하게 하면 될듯
-6. 적 클래스 추가. 일반 몹 3개, 보스몹 1개 (유미, 티모, 다리우스) / (트린다미어)
-7. 게임 시작 시 각 스테이지에서 출현할 몹들을 정할 로직.
-    배열에 1,2,3 요소가 각각 3개씩 들어가게, -> 3개 단위로 중복되지 않게 1, 2, 3을 할당
-    -> 마지막 요소는 -1 eg. [2,1,3, 1,2,3 3,1,2 ,-1] => 총 10 스테이지
-    1번 = 유미 / 2번 = 티모 / 3번 = 다리우스 / -1번 = 트린보스 // arr[stage-1]를 참조하여 적 인스턴스 생성
-8. 스테이지가 올라갈 수록 스탯 상향
-일반 몹은 스킬 1개씩만 구현... 너무 많다......
-*/
+/**
+ * @todo
+ * ㅁ1. 턴 수에 따라 지속되는 효과를 처리할 로직이 필요하다
+ * eg. Q 스킬 이속 증가 효과가 2턴 뒤에 사라져야함 / n 턴 동안 슬로우가 묻는 스킬...
+ * ㅁ2. 방어력, 이동 속도는 변동 후 원복되어야 한다. 따라서 같은 값을 참조하는 변수를 추가해야 한다.
+ * 3. 소리가 나오면 좋겠다.
+ * 4. 메시지 간 딜레이를 넣으면 좋겠다
+ * 5. 점멸 (도망) 기능. 추가 기획 필요 체력이 50% 이하일 때만 사용 가능하게, 성공확률은 이속 공식을 참조하여 회피 확률과 동일하게 하면 될듯
+ * 6. 적 클래스 추가. 일반 몹 3개, 보스몹 1개 (유미, 티모, 다리우스) / (트린다미어)
+ * 7. 게임 시작 시 각 스테이지에서 출현할 몹들을 정할 로직.
+ * 배열에 1,2,3 요소가 각각 3개씩 들어가게, -> 3개 단위로 중복되지 않게 1, 2, 3을 할당
+ * -> 마지막 요소는 -1 eg. [2,1,3, 1,2,3 3,1,2 ,-1] => 총 10 스테이지
+ * 1번 = 유미 / 2번 = 티모 / 3번 = 다리우스 / -1번 = 트린보스 // arr[stage-1]를 참조하여 적 인스턴스 생성
+ * 8. 스테이지가 올라갈 수록 스탯 상향
+ * 일반 몹은 스킬 1개씩만 구현... 너무 많다......
+ * 회피 폐기하고 점멸 도주만 구현
+ */
 
 import readlineSync from 'readline-sync';
 import chalk from 'chalk';
 
 let turn = 1;
 
+// 플레이어
 class Player {
   constructor() {
     this.maxHp = 690;
@@ -33,10 +35,14 @@ class Player {
   }
 
   applyEffect(effect) {
-    // 중복 효과 제외하고 할당
-    this.effects = this.effects.filter((e) => e.type !== effect.type);
+    // 동일한 효과가 있는 경우
+    const existingEffect = this.effects.find((e) => e.type === effect.type);
+    if (existingEffect) {
+      return;
+    }
+
     this.effects.push(effect);
-    effect.apply(this);
+    effect.applyE(this);
   }
 
   durEffect() {
@@ -44,7 +50,7 @@ class Player {
       const effect = this.effects[i];
       effect.duration--;
       if (effect.duration <= 0) {
-        effect.remove(this);
+        effect.removeE(this);
         this.effects.splice(i, 1);
       }
     }
@@ -67,8 +73,8 @@ class Player {
       new Effect(
         'Q',
         2,
-        (player) => (player.mov = player.initMov * 1.3),
-        (player) => (player.mov = player.initMov),
+        (target) => (target.mov = target.initMov * 1.3),
+        (target) => (target.mov = target.initMov),
       ),
     );
     return chalk.blue('결정타(Q)!! 다음 기본 공격이 강화되고 이동 속도가 30% 증가합니다.');
@@ -83,21 +89,64 @@ class Player {
         (player) => (player.cond = 0),
       ),
     );
-    // player.cond += 50;
     return chalk.blue(`용기(W) 맞을 용기가 생겼다. 보호막-> +${wShield}`);
   }
-  skillE() {}
-  skillR() {}
+
+  skillE(monster, eDamage) {
+    monster.applyEffect(
+      new Effect(
+        'E',
+        3,
+        (monster) => (monster.def -= monster.initDef * 0.3),
+        (monster) => (monster.def = monster.initDef),
+      ),
+    );
+    monster.hp -= eDamage;
+    return chalk.blue(
+      `심판(E) 눈도 깜짝 안 한다~!. 피해-> -${eDamage} | 방어력-> -${monster.def * 0.3}`,
+    );
+  }
+
+  skillR(monster, rDamage) {
+    monster.hp -= rDamage;
+    return chalk.blue(`데마시아의 정의(R) 한 뚝배기!! 피해-> ${rDamage}`);
+  }
 }
 
+// 적
 class Monster {
   constructor(stage) {
     this.maxHp = 10000;
     this.hp = 10000;
-    this.atk = 10;
-    this.def = 10;
+    this.initAtk = 10;
+    this.atk = this.initAtk;
+    this.initDef = 10;
+    this.def = this.initDef;
     this.mov = 300;
     this.mana = 200;
+    this.effects = []; // Effect 인스턴스를 할당하여 사용
+  }
+
+  applyEffect(effect) {
+    // 동일한 효과가 있는 경우
+    const existingEffect = this.effects.find((e) => e.type === effect.type);
+    if (existingEffect) {
+      return;
+    }
+
+    this.effects.push(effect);
+    effect.applyE(this);
+  }
+
+  durEffect() {
+    for (let i = this.effects.length - 1; i >= 0; i--) {
+      const effect = this.effects[i];
+      effect.duration--;
+      if (effect.duration <= 0) {
+        effect.removeE(this);
+        this.effects.splice(i, 1);
+      }
+    }
   }
 
   attack(player, receiveDamage) {
@@ -115,11 +164,11 @@ class Monster {
 // 턴 수 적용을 받는 스탯 변동 효과
 // param: 스킬, 지속턴수, 변동된스탯, 원복된스탯
 class Effect {
-  constructor(type, duration, apply, remove) {
+  constructor(type, duration, applyE, removeE) {
     this.type = type;
     this.duration = duration;
-    this.apply = apply;
-    this.remove = remove;
+    this.applyE = applyE;
+    this.removeE = removeE;
     // 평타 강화용
     this.active = true;
   }
@@ -146,31 +195,26 @@ function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
-// 전투 중 턴 수 영향을 받는 스탯 변동 처리
-// param: 지속턴, 계수, 변동될 스탯
-const statDuration = (duration, stregth, stat) => {
-  if (duration > 0) stat *= stregth;
-  return statDuration;
-};
-
 const battle = async (stage, player, monster) => {
   let logs = [];
-  const myDef = 1 - player.def / (player.def + 100); // 방어력으로 감소하는 피해 비율 (플레이어)
-  const enemyDef = 1 - monster.def / (monster.def + 100); // 방어력으로 감소하는 피해 비율 (적)
-
-  // ============실제 사용할 변수================
-  const receiveDamage = Math.floor(monster.atk * myDef); // 방어력 적용된 받는 대미지
-  const giveDamage = Math.floor(player.atk * enemyDef); // 방어력 적용된 주는 대미지
-  const wShield = Math.floor(player.maxHp * 0.05); // w 보호막
-  const eDamage = Math.floor(giveDamage * 0.8); // e 스킬 피해
-  // ===========================================
 
   while (player.hp > 0) {
-    console.clear();
-
+    // 효과 업데이트
     player.durEffect();
-    // 새로운 턴에 보호막 제거
-    // player.cond = null;
+    monster.durEffect();
+
+    // 방어력 업데이트
+    const myDef = 1 - player.def / (player.def + 100); // 방어력으로 감소하는 피해 비율 (플레이어)
+    const enemyDef = 1 - monster.def / (monster.def + 100); // 방어력으로 감소하는 피해 비율 (적)
+
+    // ============실제 사용할 변수================
+    const receiveDamage = Math.floor(monster.atk * myDef); // 방어력 적용된 받는 대미지
+    const giveDamage = Math.floor(player.atk * enemyDef); // 방어력 적용된 주는 대미지
+    const wShield = Math.floor(player.maxHp * 0.05); // w 보호막
+    const eDamage = Math.floor(giveDamage * 0.8); // e 스킬 피해
+    const rDamage = Math.floor((monster.maxHp - monster.hp) * 0.3); // e 스킬 피해
+    // ===========================================
+    console.clear();
 
     displayStatus(stage, player, monster);
 
@@ -210,15 +254,11 @@ const battle = async (stage, player, monster) => {
         break;
       // E 스킬
       case 'E':
-        monster.hp -= player.atk - monster.def * 2;
-        logs.push(chalk.blue(`심판(E) 눈도 깜짝 안 한다~!. 피해-> -${eDamage}`));
+        returnAct = player.skillE(monster, eDamage);
         break;
       // 궁
       case 'R':
-        monster.hp -= player.atk * 3 - monster.def;
-        logs.push(
-          chalk.blue(`플레이어가 데마시아의 정의를 사용합니다. 몬스터 체력: ${monster.hp}`),
-        );
+        returnAct = player.skillR(monster, rDamage);
         break;
       // 잘못된 입력
       default:
